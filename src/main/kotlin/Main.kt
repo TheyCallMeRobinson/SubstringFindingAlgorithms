f7730a58d2632ee1bcafc4fb31249ec0027cf753
Paul Robinson <alettertopaulrobinson0099@gmail.com>
2023-02-20 14:09:35 +0300

    Create tests for prefix border array

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,14 @@
+import algorithms.borders.PrefixBorderArray
 import algorithms.stringmatch.NaiveStringMatch
 import algorithms.stringmatch.StringMatchAlgorithm
 import java.util.Scanner
 
-fun String.indexOf(substring: String, algorithm: StringMatchAlgorithm = NaiveStringMatch()): List<Position> {
+fun String.indexOfStringMatch(substring: String, algorithm: StringMatchAlgorithm = NaiveStringMatch()): List<Position> {
     return algorithm.execute(this, substring)
+}
+
+fun String.getBorderShiftArray(algorithm: PrefixBorderArray = PrefixBorderArray()): Array<Int> {
+    return algorithm.execute(this)
 }
 
 fun Scanner.nextLine(precedingString: String): String {
@@ -11,12 +16,24 @@ fun Scanner.nextLine(precedingString: String): String {
     return next()
 }
 
-fun main() {
-    val scanner = Scanner(System.`in`)
+fun naiveStringMatchOutput(scanner: Scanner) {
     val inputBaseString = scanner.nextLine(precedingString = "Input base string: ")
     val inputSubstring = scanner.nextLine(precedingString = "Input substring: ")
-    val positions = inputBaseString.indexOf(inputSubstring)
+    val positions = inputBaseString.indexOfStringMatch(inputSubstring)
     for (i in positions) {
         println(i)
     }
+}
+
+fun prefixBorderArrayOutput(scanner: Scanner) {
+    val inputString = scanner.nextLine(precedingString = "Input string: ")
+    val array = inputString.getBorderShiftArray()
+    for (i in array) {
+        println(i)
+    }
+}
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    prefixBorderArrayOutput(scanner)
 }
