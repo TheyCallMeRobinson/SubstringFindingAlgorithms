585f8eca13114c4ff6c37a6f16d2118b2d0cf9a5
Maxim <nikitin@redcollar.ru>
2023-03-28 15:50:56 +0300

    Added shiftAnd algorithm

--- src/main/kotlin/algorithms/stringmatch/ShiftAndMatch.kt
+++ src/main/kotlin/algorithms/stringmatch/ShiftAndMatch.kt
@@ -2,27 +2,25 @@ package algorithms.stringmatch
 
 import Position
 
-class ShiftAndMatch : StringMatchAlgorithm{
+class ShiftAndMatch : StringMatchAlgorithm {
     override fun execute(baseString: String, substring: String): List<Position> {
-        val mapEntries = mutableMapOf<Char, Int>()
+        val mapEntries = mutableMapOf<Char, Long>()
         val positionList = mutableListOf<Position>()
 
-        for (i in substring.indices) {
-            mapEntries[substring[i]] = 1 shl (substring.length - 1 - i)
-        }
+        for(i in 0..255) mapEntries[i.toChar()] = 0L
+        for (i in substring.indices) mapEntries[substring[i]] = mapEntries[substring[i]]!! or (1L shl (substring.length - 1 - i))
 
-        val uHigh = 1 shl (substring.length - 1)
-        var mask = 0
+        val uHigh = 1L shl (substring.length - 1)
+        var mask = 0L
 
-        for(i in baseString.indices) {
-            val code = if(mapEntries[baseString[i]] == null) 0 else mapEntries[baseString[i]]
+        for (i in baseString.indices) {
+            val code = if (mapEntries[baseString[i]] == null) 0 else mapEntries[baseString[i]]
             mask = (mask shr 1 or uHigh) and code!!
 
-            if(mask and 1 != 0) {
+            if (mask and 1 != 0L) {
                 positionList.add(Position(i - substring.length + 1))
             }
         }
-
         return positionList
     }
 }
